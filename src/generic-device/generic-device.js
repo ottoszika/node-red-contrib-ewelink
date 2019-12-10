03c56aa2c1169e928fe043e85154bc75a5d05a67
Otto Szika <ottoszika@users.noreply.github.com>
2019-12-10 20:52:39 +0200

    Extract common device node logic (#34)
    * Cleanup code
    
    * Cleanup code
    
    * Extract common device node logic

--- src/generic-device/generic-device.js
+++ src/generic-device/generic-device.js
@@ -1,4 +1,4 @@
-var eWeLinkConnect = require('../utils/ewelink-connect');
+const eWeLinkConnect = require('../utils/ewelink-connect');
 
 /**
  * Generic device node.
@@ -13,27 +13,8 @@ module.exports = (RED) => {
     // Create the node
     RED.nodes.createNode(this, config);
 
-    // Clean up device ID
-    const deviceId = config.deviceId ? config.deviceId.trim() : '';
-    
-    // Log in to eWeLink
-    eWeLinkConnect.login(RED, this, config).then(connection => {
-      // Once logged in we can listen to inputs
-      this.on('input', (msg) => {
-        let { method, params } = msg.payload;
-
-        // Set params to empty array if not set
-        params = params || [];
-
-        // First parameter should be always the device ID
-        params.unshift(deviceId);
-        
-        // Call dynamically the method
-        connection[method].apply(connection, params).then(result => {
-          this.send({ payload: result });
-        }).catch(error => this.error(error));
-      })
-    }).catch(error => this.error(error));
+    // Initialize device node
+    eWeLinkConnect.initializeDeviceNode(RED, this, config);
   }
 
   // Register node
--- src/utils/ewelink-connect.js
+++ src/utils/ewelink-connect.js
@@ -50,6 +50,37 @@ module.exports = {
   },
 
   /**
+   * Initialize device node.
+   * 
+   * @param {object} RED The NodeRED instance.
+   * @param {object} node The current node.
+   * @param {object} config The node configuration.
+   * @param {string} method The method to call on the device.
+   * @param {array} params The parameters of the method.
+   */
+  initializeDeviceNode(RED, node, config, method, params) {
+    // Clean up device ID
+    const deviceId = config.deviceId ? config.deviceId.trim() : '';
+
+    // Log in to eWeLink
+    this.login(RED, node, config).then(connection => {
+      // Once logged in we can listen to inputs
+      node.on('input', (msg) => {
+        method = method || msg.payload.method;
+        params = params || msg.payload.params || [];
+
+        // First parameter should be always the device ID
+        params.unshift(deviceId);
+        
+        // Call dynamically the method
+        connection[method].apply(connection, params).then(result => {
+          node.send({ payload: result });
+        }).catch(error => node.error(error));
+      })
+    }).catch(error => node.error(error));
+  },
+
+  /**
    * Set node status to 'connecting'.
    * 
    * @param {object} node The node which status will be changed.
