91a9ddf58574264f9749278477ef0ab48dc6e619
Otto Szika <ottoszika@users.noreply.github.com>
2019-12-11 23:53:57 +0200

    Separate power state read and write nodes (#47)

---
+++ src/power-state-read/power-state-read.js
@@ -0,0 +1,22 @@
+const eWeLinkConnect = require('../utils/ewelink-connect');
+
+/**
+ * Power state read node.
+ */
+module.exports = (RED) => {
+  /**
+   * Power state read node constructor.
+   * 
+   * @param {object} config The node configuration.
+   */
+  function PowerStateReadNode(config) {
+    // Create the node
+    RED.nodes.createNode(this, config);
+
+    // Initialize device node
+    eWeLinkConnect.initializeDeviceNode(RED, this, config, 'getDevicePowerState');
+  }
+
+  // Register node
+  RED.nodes.registerType('ewelink-power-state-read', PowerStateReadNode);
+}
